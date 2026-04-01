42d7cb0dfb0458fcdfde0a0c3bc81f42d93e3e99
John Doe <johndoe@example.com>
2026-04-01 22:51:24 +0800

    修改了默认后端的URL地址，替换为新的URL地址。

--- workers.js
+++ workers.js
@@ -16,17 +16,17 @@ const ADMIN_PASSWORD = "admin123"; // 请修改为强密码！
 const DEFAULT_BACKENDS = {
     "8443": {
         name: "Emby 2",
-        url: "https://link00.okemby.org:8443",
+        url: "https://XXX.YYY.ZZZ:8443",
         enabled: true
     },
     "2053": {
         name: "Emby 3",
-        url: "https://www.lilyemby.com",
+        url: "https://XXX.YYY.ZZZ",
         enabled: true
     },
     "default": {
         name: "Emby 1 (默认)",
-        url: "http://wf.vban.com:8880",
+        url: "http://XXX.YYY.ZZZ:8880",
         enabled: true
     }
 };
